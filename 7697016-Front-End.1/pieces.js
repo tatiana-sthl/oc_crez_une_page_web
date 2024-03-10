ba60b094e0c60df87c23f07832023f29e9e32ce9
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2024-03-10 16:45:53 +0100

    P3C3

--- 7697016-Front-End.1/pieces.js
+++ 7697016-Front-End.1/pieces.js
@@ -1,8 +1,10 @@
-import { ajoutListenersAvis } from "./avis.js";
+import { ajoutListenersAvis, ajoutListenerEnvoyerAvis } from "./avis.js";
 
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('http://localhost:8081/pieces/');
 const pieces = await reponse.json();
+
+ajoutListenerEnvoyerAvis()
 
 function genererPieces(pieces){
     for (let i = 0; i < pieces.length; i++) {
